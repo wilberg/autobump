18779becd7a3579ff1f55eae2453c00f89fe3f55
Birk I. Wilberg <birk@wilberg.dev>
2024-02-25 00:07:53 +0100

    make compile method smaller

--- packages/compiler/src/compile.ts
+++ packages/compiler/src/compile.ts
@@ -3,7 +3,5 @@ import { parse } from '@tagup/parser';
 import { Adapter } from './types/adapter';
 
 export function compile<T>(source: string, adapter: Adapter<T>): T {
-    const tokens = scan(source, ['for', 'if', 'else', 'in']);
-    const program = parse(tokens);
-    return adapter(program);
+    return adapter(parse(scan(source, ['for', 'if', 'else', 'in'])));
 }
