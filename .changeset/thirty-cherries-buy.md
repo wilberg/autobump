c75d8d87a8658fe12cf0a39f7f2ab2dc3b3b1adf
Birk I. Wilberg <birk@wilberg.dev>
2024-03-02 17:43:54 +0100

    add syntax highlighting for use and in

---
+++ .changeset/thirty-cherries-buy.md
@@ -0,0 +1,5 @@
+---
+"tagup-language-support": patch
+---
+
+Add syntax highlighting for use and in
--- packages/compiler/src/compile.ts
+++ packages/compiler/src/compile.ts
@@ -4,5 +4,5 @@ import { Adapter } from './types/adapter';
 import { JSAdapter } from '.';
 
 export function compile<T = string>(source: string, adapter: Adapter<T> = JSAdapter as Adapter<T>): T {
-    return adapter(parse(scan(source, ['for', 'if', 'else', 'in'])));
+    return adapter(parse(scan(source, ['for', 'if', 'else', 'in', 'use'])));
 }
